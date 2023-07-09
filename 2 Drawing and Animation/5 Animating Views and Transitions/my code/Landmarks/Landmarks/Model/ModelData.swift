384651cc6b98fc5fd1f119d56850d72d91318686
Vitaliy <>
2023-07-09 20:00:28 +0300

    Section 2 Add Animations to Individual Views

--- 2 Drawing and Animation/5 Animating Views and Transitions/my code/Landmarks/Landmarks/Model/ModelData.swift
+++ 2 Drawing and Animation/5 Animating Views and Transitions/my code/Landmarks/Landmarks/Model/ModelData.swift
@@ -11,6 +11,9 @@ import Combine
 
 final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
+    
+    //Because you’ll never modify hike data after initially loading it, you don’t need to mark it with the @Published attribute.
+    var hikes: [Hike] = load("hikeData.json")
 }
 
 
