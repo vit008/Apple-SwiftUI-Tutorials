b117c0157ed4f25617f7b7fa1595fae0f8aad8dc
Vitaliy <>
2023-07-09 21:14:56 +0300

    Section 5 Add Navigation Between Sections
    finished

--- 3 App Design and Layout/6 Composing Complex Interfaces/my code/Landmarks/Views/ContentView.swift
+++ 3 App Design and Layout/6 Composing Complex Interfaces/my code/Landmarks/Views/ContentView.swift
@@ -1,15 +1,38 @@
 /*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-A view showing the list of landmarks.
-*/
+ See LICENSE folder for this sample’s licensing information.
+ 
+ Abstract:
+ A view showing the list of landmarks.
+ */
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selection: Tab = .featured
+    
+    
+    enum Tab {
+        case featured
+        case list
+    }
+    
+    
     var body: some View {
-        LandmarkList()
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem {
+                    Label("Featured", systemImage: "star")
+                }
+                .tag(Tab.featured)
+            
+            
+            LandmarkList()
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+        }
     }
 }
 
