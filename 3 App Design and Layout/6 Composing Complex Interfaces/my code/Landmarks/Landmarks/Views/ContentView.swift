bea4400d06fb12d9e583ed958ee4e2533eb47bb0
Vitaliy <>
2023-07-09 20:23:40 +0300

    add my code from previous lesson

---
+++ 3 App Design and Layout/6 Composing Complex Interfaces/my code/Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Vitaliy Fedun on 09.07.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
