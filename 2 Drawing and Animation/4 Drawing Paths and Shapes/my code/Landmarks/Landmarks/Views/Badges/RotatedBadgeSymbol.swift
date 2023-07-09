d0d06944f6c0e67d77e218c545bcdeedaa99eaf4
Vitaliy <>
2023-07-09 19:43:03 +0300

    Section 3 Draw the Badge Symbol done

---
+++ 2 Drawing and Animation/4 Drawing Paths and Shapes/my code/Landmarks/Landmarks/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Vitaliy Fedun on 09.07.2023.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
