2582ec24c7d4254d351274c3186c61a66265f930
Vitaliy <>
2023-07-09 20:03:34 +0300

    Section 3 Animate the Effects of State Changes

--- 2 Drawing and Animation/5 Animating Views and Transitions/my code/Landmarks/Landmarks/Views/Hikes/HikeView.swift
+++ 2 Drawing and Animation/5 Animating Views and Transitions/my code/Landmarks/Landmarks/Views/Hikes/HikeView.swift
@@ -1,32 +1,34 @@
 /*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-A view displaying information about a hike, including an elevation graph.
-*/
+ See LICENSE folder for this sample’s licensing information.
+ 
+ Abstract:
+ A view displaying information about a hike, including an elevation graph.
+ */
 
 import SwiftUI
 
 struct HikeView: View {
     var hike: Hike
     @State private var showDetail = false
-
+    
     var body: some View {
         VStack {
             HStack {
                 HikeGraph(hike: hike, path: \.elevation)
                     .frame(width: 50, height: 30)
-
+                
                 VStack(alignment: .leading) {
                     Text(hike.name)
                         .font(.headline)
                     Text(hike.distanceText)
                 }
-
+                
                 Spacer()
-
+                
                 Button {
-                    showDetail.toggle()
+                    withAnimation {
+                        showDetail.toggle()
+                    }
                 } label: {
                     Label("Graph", systemImage: "chevron.right.circle")
                         .labelStyle(.iconOnly)
@@ -38,7 +40,7 @@ struct HikeView: View {
                         .animation(.spring(), value: showDetail)
                 }
             }
-
+            
             if showDetail {
                 HikeDetail(hike: hike)
             }
