ce4488a33d52daf8fc62fb44d8dc192c5213e554
Vitaliy <>
2023-07-09 20:07:06 +0300

    Section 4 Customize View Transitions

--- 2 Drawing and Animation/5 Animating Views and Transitions/my code/Landmarks/Landmarks/Views/Hikes/HikeView.swift
+++ 2 Drawing and Animation/5 Animating Views and Transitions/my code/Landmarks/Landmarks/Views/Hikes/HikeView.swift
@@ -7,6 +7,23 @@
 
 import SwiftUI
 
+
+extension AnyTransition {
+    static var moveAndFade: AnyTransition {
+
+
+        //AnyTransition.slide
+
+        //AnyTransition.move(edge: .trailing)
+        
+        .asymmetric(
+                    insertion: .move(edge: .trailing).combined(with: .opacity),
+                    removal: .scale.combined(with: .opacity)
+                )
+    }
+}
+
+
 struct HikeView: View {
     var hike: Hike
     @State private var showDetail = false
@@ -43,6 +60,7 @@ struct HikeView: View {
             
             if showDetail {
                 HikeDetail(hike: hike)
+                    .transition(.moveAndFade)
             }
         }
     }
