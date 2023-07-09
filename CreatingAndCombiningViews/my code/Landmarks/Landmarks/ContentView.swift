b7b0701cd6b4178967116ebf1af795e72d56b0d6
Vitaliy <>
2023-07-09 14:33:29 +0300

    Section 2 "Customize the Text View" done

--- CreatingAndCombiningViews/my code/Landmarks/Landmarks/ContentView.swift
+++ CreatingAndCombiningViews/my code/Landmarks/Landmarks/ContentView.swift
@@ -9,13 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world FV!")
-        }
-        .padding()
+        
+        Text("Turtle Rock")
+            .font(.title)
+            
+        
     }
 }
 
